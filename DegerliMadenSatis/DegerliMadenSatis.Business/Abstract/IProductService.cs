c81eb115a34cf7d9fa96ea345ea2cfa6d51da22c
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-03-19 01:55:27 +0300

    ınterfacestart

---
+++ DegerliMadenSatis/DegerliMadenSatis.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using DegerliMadenSatis.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerliMadenSatis.Business.Abstract
+{
+    public interface IProductService
+    {
+        //Interface özeliği gereği sadece metot imzaları alır, gövdesi yer almaz. Burda miras alınan class ta yazılır.
+        //Produck CRUD işlemlerinin metotları burada yazılır.
+        List<Product> GetAll(bool? isHome, bool? isActive, bool? isDelete); //? nullable yapıyor.
+    }
+}
