057d94ff7d55bbe0f833ffad820230246817638e
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-03-18 13:21:25 +0300

    mig

--- DegerliMadenSatis/DegerliMadenSatis.Data/AppDbContext.cs
+++ DegerliMadenSatis/DegerliMadenSatis.Data/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using DegerliMadenSatis.Entity;
@@ -11,6 +12,7 @@ namespace DegerliMadenSatis.Data
 {
     public class AppDbContext : DbContext
     {
+        //Veri tabanının kalbi katagoriler ile ürünler burada eşleniyor.
         public DbSet<Category> Categories  {get; set;}
         public DbSet<Product> Products { get; set; }
         public DbSet<CategoryProduct> CategoryProducts { get; set; }
@@ -18,6 +20,14 @@ namespace DegerliMadenSatis.Data
         {
             optionsBuilder.UseSqlServer("");
             base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)//Katagoriler ile ürünlere primary key atama işlemi
+        {
+            modelBuilder.Entity<CategoryProduct>().HasKey(x=> new //Çok fazla katagori olabilir bir döngüye sokulup her bir katagori için uygula dedik.
+            { x.CategoryId, x.ProductId});
+               
+            
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
