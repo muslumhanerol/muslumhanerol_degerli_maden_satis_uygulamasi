61820a67cfe2e5a52eecae21c582c9798988d1cc
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-03-20 22:02:32 +0300

    data_crud

---
+++ DegerliMadenSatis/DegerliMadenSatis.Data/Abstract/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerliMadenSatis.Data.Abstract
+{
+    public interface IGenericRepository<TEntity> //Veri tabanı işlemlerini bu kısımda barındırılır. (CRUD)
+    {
+        List<TEntity> GetAll();//Dışarıdan category produck gelecek, burada crud işlemleri gerçekleşecek.
+        TEntity GetById(int id);
+        void Create(TEntity entity); 
+        void Update(TEntity entity);
+        void HardDelete(TEntity entity);
+        void SoftDelete(TEntity entity);
+
+    }
+}
