e30b6ca4b0e99b30fdf625c73820c64a3e848cdd
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-03-20 23:39:09 +0300

    productrepos

---
+++ DegerliMadenSatis/DegerliMadenSatis.Data/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using DegerliMadenSatis.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerliMadenSatis.Data.Abstract
+{
+    public interface IProductRepository:IGenericRepository<Product> //IGenericRepository de product la ilgili ne varsa tamamı IProductRepository için de var.
+    {
+        //Product'a özgü metot imzaları buraya yazacağız.
+    }
+}
