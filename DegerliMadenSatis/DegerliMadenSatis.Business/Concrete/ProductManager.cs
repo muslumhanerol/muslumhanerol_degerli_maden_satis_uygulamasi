6aa0e4dc897e03c3d6bed84140645a6ea580307b
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-03-25 00:03:57 +0300

    foreachproduct

--- DegerliMadenSatis/DegerliMadenSatis.Business/Concrete/ProductManager.cs
+++ DegerliMadenSatis/DegerliMadenSatis.Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using DegerliMadenSatis.Business.Abstract;
 using DegerliMadenSatis.Core.ViewModels;
 using DegerliMadenSatis.Data.Abstract;
+using DegerliMadenSatis.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +26,21 @@ namespace DegerliMadenSatis.Business.Concrete //bu bölümde IProductService den
 
         public List<ProduckViewModel> GetAll(bool? isHome, bool? isActive, bool? isDelete) //isHome u true olanları göstermek.
         {
-
+            var products = _productRepository.GetAll();
+            List<ProduckViewModel> produckViewModels = new List<ProduckViewModel>();
+            ProduckViewModel produckViewModel;            
+            foreach (var product in products)
+            {
+                produckViewModel = new ProduckViewModel
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Price = product.Price,
+                    ImageUrl = product.ImageUrl,
+                    Properties = product.Properties,
+                    Url = product.Url
+                };
+            }
         }
 
         public ProduckViewModel GetById(int id)
