3b95102ca82df28dacba1cf92fd8ccaf97cc43e1
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-04-02 01:58:05 +0300

    roleexample

--- DegerliMadenSatiss/DegerliMadenSatis.Data/Extensions/ModelBuilderExtensions.cs
+++ DegerliMadenSatiss/DegerliMadenSatis.Data/Extensions/ModelBuilderExtensions.cs
@@ -21,7 +21,43 @@ namespace DegerliMadenSatis.Data.Extensions
             };
             modelBuilder.Entity<Role>().HasData(role); //Entity içerisinden role geç hasdata yı kullanarak listedeki tanımlı rolleri ver.
 
-        #endregion
-    }
+            #endregion
+
+            #region Kullanıcı Bilgileri
+            List<User> users = new List<User>
+            {
+                new User //Örnek datamız.
+                {
+                    FirstName="Müslüm Han",
+                    LastName="Erol",
+                    UserName="muslumhanerol",
+                    NormalizedUserName="MUSLUMHANEROL",
+                    Email="mslmhanerol@gmail.com",
+                    NormalizedEmail="MSLMHANEROL@GMAIL.COM",
+                    Gender="Erkek",
+                    DateOfBirth=new DateTime(1945,1,11),
+                    Address="Gaziantep Caddesi Gaziantep Sokak No:27 D:27 Türkiye",
+                    City="Gaziantep",
+                    PhoneNumber="5554443322",
+                    EmailConfirmed=true
+                },
+                 new User 
+                {
+                    FirstName="Engin",
+                    LastName="Niyazi",
+                    UserName="enginniyazi",
+                    NormalizedUserName="ENGINNIYAZI",
+                    Email="enginniyazi@gmail.com",
+                    NormalizedEmail="ENGINNIYAZI@GMAIL.COM",
+                    Gender="Erkek",
+                    DateOfBirth=new DateTime(1965,7,20),
+                    Address="Gaziantep Caddesi Gaziantep Sokak No:27 D:27 Türkiye",
+                    City="Gaziantep",
+                    PhoneNumber="5556667788",
+                    EmailConfirmed=true
+                },
+            };
+            #endregion
+        }
     }
 }
