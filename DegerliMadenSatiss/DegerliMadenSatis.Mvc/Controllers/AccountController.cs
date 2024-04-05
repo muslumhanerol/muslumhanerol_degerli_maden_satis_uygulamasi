0ff1c37c7cc7d0edb90504f287cba7fbcf1ed294
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-04-05 18:06:21 +0300

    form_ok

--- DegerliMadenSatiss/DegerliMadenSatis.Mvc/Controllers/AccountController.cs
+++ DegerliMadenSatiss/DegerliMadenSatis.Mvc/Controllers/AccountController.cs
@@ -1,11 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DegerliMadenSatis.Entity.Concrete.identity;
+using DegerliMadenSatis.Shared.ViewModels.IdentityModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DegerliMadenSatis.MVC.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Register()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel) // Register formdan gelen verileri karşılayacak.
+        {
+            if (ModelState.IsValid) //Formdaki veriler RegisterViewModel da belirtilen kurallara uygun girilmiş mi girilmemiş mi kontrolü yapılır.
+            {
+                User user = new User
+                {
+                    UserName = registerViewModel.UserName,
+                    Email = registerViewModel.Email,
+                    FirstName = registerViewModel.FirstName,
+                    LastName = registerViewModel.LastName,
+                    
+                };
+                var result = await _userManager.CreateAsync(user,registerViewModel.Password); //Kullanıcını girdiği şifreyi şifrelen algaritmayı çalıştırmak.
+                if (result.Succeeded)
+                {
+                    /*return RedirectToAction("Index","Home");*/ //HomeControllerın indexine git.
+                    return Redirect("~/"); //İkinci yöntem
+                }
+            }
             return View();
         }
     }
--- DegerliMadenSatiss/DegerliMadenSatis.Shared/ViewModels/IdentityModels/RegisterViewModel.cs
+++ DegerliMadenSatiss/DegerliMadenSatis.Shared/ViewModels/IdentityModels/RegisterViewModel.cs
@@ -7,26 +7,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DegerliMadenSatis.Shared.ViewModels.IdentityModels
-{
+{//Register Kısmı İlk adım. İkinci adım MVC>Views>Account>Register
+ //Girilecek bilgileri karşlama modeli. bunda sonra MVC>Views>Account>Register kısmında bu modeli yazmalıyız.
     public class RegisterViewModel
     {
         [DisplayName("Ad")] //Kullanıcının göreceği adlandırma.
         [Required (ErrorMessage ="Lütfen ad alanını boş bırakmayınız.")] //İsim alanı zorunlu olsun dedik.       
         public string FirstName { get; set; }
+        
+        
         [DisplayName("Soyad")]
         [Required(ErrorMessage = "Lütfen soyad alanını boş bırakmayınız.")]
         public string LastName { get; set; }
+        
+        
         [DisplayName("Kullanıcı Adı")]
         [Required(ErrorMessage = "Lütfen kullanıcı adı alanını boş bırakmayınız.")]
         public string UserName { get; set; }
+        
+        
         [DisplayName("Email")]
         [Required(ErrorMessage = "Lütfen email alanını boş bırakmayınız.")]
         [DataType(DataType.EmailAddress,ErrorMessage ="Lütfen geçerli bir email adresi giriniz.")] //Girilecek bilginin email adresi olma zorunluluğu getirir.
         public string Email { get; set; }
+        
+        
         [DisplayName("Parola")]
         [Required(ErrorMessage = "Lütfen parola alanını boş bırakmayınız.")]
         [DataType(DataType.Password)] //Şifre *** şeklinde görünecek
         public string Password { get; set; }
+        
         
         [DisplayName("Parola Tekrar")]
         [Required(ErrorMessage = "Lütfen parola tekrar alanını boş bırakmayınız.")]
