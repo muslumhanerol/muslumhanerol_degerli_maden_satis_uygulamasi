f110dfda9b4bb45dba9fd30b0556ee3191b9ea78
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-04-06 00:05:37 +0300

    login_starts

---
+++ DegerliMadenSatiss/DegerliMadenSatis.Shared/ViewModels/IdentityModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerliMadenSatis.Shared.ViewModels.IdentityModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DisplayName("Kullanıcı Adı")]
+        public string UserName { get; set; }
+        [Required]
+        [DisplayName("Parola")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
