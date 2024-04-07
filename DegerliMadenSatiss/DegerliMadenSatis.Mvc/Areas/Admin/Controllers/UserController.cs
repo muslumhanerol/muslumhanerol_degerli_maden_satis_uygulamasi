a6db40c7455fa9661d54b4de2371b27e06304ef3
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-04-08 01:31:19 +0300

    q

---
+++ DegerliMadenSatiss/DegerliMadenSatis.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using DegerliMadenSatis.Entity.Concrete.identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DegerliMadenSatis.MVC.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+
+        public UserController(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        //public async Task<IActionResult> Index() //1.Yöntem 
+        //{
+        //    var users = await _userManager.Users.ToListAsync();
+        //    return View(users);
+        //}
+        public async Task<IActionResult> Index() => View(await _userManager.Users.ToListAsync()); //2. Yöntem
+    }
+}
